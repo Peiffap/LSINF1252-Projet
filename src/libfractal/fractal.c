bfca8c28cc0795c262d175e43f7220e6ee62092c
Лилия Семерикова <liliasemerikova@wifi-student4-53.sri.ucl.ac.be>
2018-04-13 10:42:38 +0200

    changement

--- src/libfractal/fractal.c
+++ src/libfractal/fractal.c
@@ -17,6 +17,7 @@ struct fractal *fractal_new(const char *name, int width, int height, double a, d
     data = malloc(width * sizeof(int *)); // Pointer vector
     int i;
     for (i = 0; i < width; i++)
+        //apzieruhaerughaeporighaeporiga
     {
         data[i] = malloc(height * sizeof(int)); // Allocate each entry in the array allocated above
     }
