826722c12d23715b11ecb11448d487414d8c309c
Peiffap <gilles.peiffer@hotmail.com>
2018-04-17 09:55:24 +0200

    implementing -d, wip

--- src/main.c
+++ src/main.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include "fractal.h"
 
-int main()
+int main(int argc, const char *argv[])
 {
     /* TODO */
+    
+    const char d[3] = "-d";
 
+    if (argc > 2 && strcmp(argv[1], d))
+    
     return 0;
 }
