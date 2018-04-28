a596b44163d8ed96ab676ebe775407f376453f48
Peiffap <gilles.peiffer@hotmail.com>
2018-04-28 15:16:48 +0200

    make argument positioning less restrictive

--- src/main.c
+++ src/main.c
@@ -4,10 +4,15 @@
 #include <unistd.h>
 #include <stdbool.h>
 #include <math.h>
-#include "fractal.h"
+#include <pthread.h>
+#include <semaphore.h>
+#include "libfractal/fractal.h"
 
-bool generateAll = false; // Determines whether all .bmp files need to be generated
-bool commandLineInput = false; // Determines whether the user is gonna enter fractals from the command line
+#define NUMBER_OF_THREADS 7
+
+bool generateAll = false; // Determines whether all .bmp files need to be generated.
+bool commandLineInput = false; // Determines whether the user is gonna enter fractals from the command line.
+
 
 int main(int argc, const char *argv[])
 {
@@ -15,44 +20,32 @@ int main(int argc, const char *argv[])
     
     const char dArg[3] = "-d";
     const char maxThreadsArg[13] = "--maxthreads";
+    const char hyphenArg[2] = "-";
     
-    int searchIndex = 1; // Index to search for arguments in argv
-    int maxThreads = 7; // Test for optimal number of threads
+    int maxThreads = NUMBER_OF_THREADS; // Test for optimal number of threads.
+    bool assignedMaxThreads = false;
     
-    
-    
-    // [--maxthreads n] can be the second and third arguments (first argument being the name of the program) or the third and fourth (because -d might take up one spot)
-    // If there is a specification for the maximal number of threads, but the value of n is less than one, one thread is used by default
-    if (argc > 2 && strcmp(argv[1], maxThreadsArg) == 0)
-    {
-        maxThreads = fmax(1, (int) strtol(argv[2], (char **)NULL, 10));
-        searchIndex += 2;
-    }
-    else if (argc > 3 && strcmp(argv[2], maxThreadsArg) == 0)
-    {
-        maxThreads = fmax(1, (int) strtol(argv[3], (char **)NULL, 10));
-        searchIndex += 2;
-    }
-    
-
-    
-    // [-d] can be the second argument (first argument being the name of the program) or the fourth (because [--maxthreads n] might take up two spots)
-    if ((argc > 2 && strcmp(argv[1], dArg) == 0) ||
-        (argc > 4 && strcmp(argv[3], dArg) == 0))
-    {
-        generateAll = true;
-        searchIndex++;
-    }
-    
-    
-    
-    // If one of the input files is -, the user will be inputting fractals from the command line
     int i;
-    for (i = searchIndex; i < argc - 1; ++i){
-        if (strcmp(argv[i], "-") == 0)
+    // Stop iterating once the loop reaches the output file which is always last or when the 3 modifiers have been set already, therefore not needing to iterate any longer.
+    for (i = 1; i < argc - 1 && (!commandLineInput && !generateAll && !assignedMaxThreads); ++i)
+    {
+        // If one of the input files is -, the user will be inputting fractals from the command line (possibly among other input methods).
+        if (strcmp(argv[i], hyphenArg) == 0)
         {
             commandLineInput = true;
-            break;
+        }
+        // [-d] is an argument determining whether all the input fractals need to be transformed into .bmp files by setting the value of the generateAll boolean.
+        else if (strcmp(argv[i], dArg) == 0)
+        {
+            generateAll = true;
+        }
+        // [--maxthreads n] determines the maximal number of threads the program is allowed to use.
+        // If there is a specification for the maximal number of threads, but the value of n is less than one, one thread is used by default. If there is no specification at all the default value is 7, because that number seems to be optimal.
+        else if (strcmp(argv[i], maxThreadsArg) == 0)
+        {
+            maxThreads = fmax(1, (int) strtol(argv[i + 1], (char **) NULL, 10));
+            ++i;
+            assignedMaxThreads = true;
         }
     }
     
