92d70b475108b85ff87191ccf2ddb59399eea1e7
gp <gilles.peiffer@hotmail.com>
2018-05-03 21:30:32 +0200

    Added push function to fractstack.c.

---
+++ src/fractstack/fractstack.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include "../libfractal/fractal.h"
+#include "fractstack.h"
+
+static pthread_mutex_t stack_mutex;
+static sem_t empty;
+static sem_t full;
+static node *head = NULL;
+
+/**
+ * Adds a fractal to the stack.
+ *
+ * @param f is a fractal to be added to the stack.
+ *
+ * @return If the fractal is succesfully pushed onto the stack, returns 0. If not, the returned value is -1.
+ */
+int push(struct fractal *f)
+{
+	node *new_node = malloc(sizeof node);
+
+	if (new_node == NULL)
+	{
+		printf("Error with malloc in push. \n");
+		return -1;
+	}
+
+	if (f == NULL){
+		printf("Error, NULL fractal in push. \n");
+		return -1;
+	}
+
+	new_node->f = f;
+
+	sem_wait(&empty);
+	pthread_mutex_lock(&stack_mutex);
+	new_node->next = head;
+	head = new_node;
+	pthread_mutex_unlock(&stack_mutex);
+	sem_post(&full);
+	
+	return 0;
+}
